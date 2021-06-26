9729c94eab6544585a7e53e834711143cfe0e353
SK <sksinghcodes@gmail.com>
2021-06-27 02:50:41 +0530

    commit project

--- js/script.js
+++ js/script.js
@@ -1,41 +1,125 @@
 var dataArray = [];
 var addBtn = document.querySelector(".addBtn");
 var input = document.querySelector(".input");
-var item = document.querySelectorAll(".listItem");
-var todoList = document.querySelector(".todoList"); 
-var capturedItem;
-var unfocusItem;
+var todoList = document.querySelector(".todoList");
 
+// check local storage and render data on the screen
 if(localStorage.length){
 	dataArray = JSON.parse(localStorage.getItem("dataString"));
+    renderTasks(dataArray)
+}
 
-	for(var i = 0; i < dataArray.length; i++ ){
-		taskUpdate(i + 1, dataArray[i].task);
-		updateList();
-		retick();
+addBtn.onclick = function(){addNewTask(input.value);}
+
+input.onkeypress = function(e){
+    if(e.key === "Enter") {
+        addBtn.click();
+        triggerClass(addBtn, 'active');
+    }
+}
+
+todoList.onclick = function(e){controlSystem(e);}
+
+function renderTasks(dataArray) {
+    for(var i = 0; i < dataArray.length; i++ ){
+		renderSingleTask(dataArray[i].task, i + 1);
 	}
 }
 
+function renderSingleTask(task, index){
+	todoList.innerHTML +=
+	`<div class="listItem">
+		<div class="checkBoxDiv">
+			<input type="checkbox" class="checkbox" onchange="toggleTask(event)" id="item${index}" >
+		</div>
+		<label for="item${index}" class="label">${task}</label>			
+		<input type="text" class="labelInput hidden" onfocusout="unfocused(event)" value="${task}">
+		<div class="controls">
+			<button class="editBtn"><span class="fa fa-pen"></span></button>
+			<button class="saveBtn hidden"><span class="fa fa-save"></span></button>
+			<button class="deleteBtn"><span class="fa fa-trash"></span></button>
+		</div>
+	</div>`;
 
-addBtn.onclick = function(){
+    setTimeout(() => {
+        renderToggleTask(index - 1)
+    }, 10);
+}
 
-	input.value = input.value.replace(/\s+/g, " ").replace(/^\s|\s$/g, "");
+function addNewTask(task){
+    // remove extra spaces from string
+    var filteredTask = filterText(task);
 
-	if( input.value && !(input.value === "  ") ){
+    if(filteredTask){
+        // render task to the screen
+        renderSingleTask(filteredTask, dataArray.length);
 
-		dataArray.push({
-			task: input.value,
-			isCompleted: false
-		});
-		taskUpdate(todoList.childElementCount + 1, input.value);
-	}
+        // update the array
+        dataArray.push({
+            task: filteredTask,
+            isCompleted: false,
+        })
 
-	input.value = "";
-	updateList();
-	retick();
-};
+        saveChangesToLocal();
+        input.value = "";
+        input.focus();
+    }
+}
 
-function saveToLocal(){
+function deleteTask(index){
+    todoList.children[index].remove();
+    dataArray.splice(index, 1);
+    saveChangesToLocal();
+    updateAttributes(index);
+}
+
+function triggerEditTask(formInput){
+    formInput.focus();
+    formInput.selectionStart = formInput.value.length;
+}
+
+function saveEditTask(index, label, formInput){
+    let filteredTask = filterText(formInput.value);
+
+    if(filteredTask) {
+        label.innerText = filteredTask;
+        dataArray[index].task = filteredTask;
+        saveChangesToLocal();
+    }
+}
+
+function toggleClass(elementArray, className){
+    for (let i = 0; i < elementArray.length; i++) {
+        if(elementArray[i].classList.contains(className)) {
+            elementArray[i].classList.remove(className);
+        } else {
+            elementArray[i].classList.add(className);
+        }
+    }
+}
+
+function toggleTask(e){
+    let index = isOnIndex(e.path[2]);
+    dataArray[index].isCompleted = !dataArray[index].isCompleted;
+    renderToggleTask(index);
+    saveChangesToLocal();
+}
+
+function renderToggleTask(index){
+    if(dataArray[index].isCompleted) {
+        todoList.children[index].children[1].classList.add('done');
+        todoList.children[index].children[0].children[0].checked = true;
+    } else {
+        todoList.children[index].children[1].classList.remove('done');
+        todoList.children[index].children[0].children[0].checked = false;
+    }
+}
+
+function filterText(text) {
+    return text.replace(/\s+/g, " ").replace(/^\s|\s$/g, "");
+}
+
+function saveChangesToLocal(){
 	if (dataArray.length){
 		localStorage.setItem("dataString", JSON.stringify(dataArray));
 	} else {
@@ -43,126 +127,73 @@ function saveToLocal(){
     }
 }
 
-document.onkeydown = function(event){
-    if(event.key === "Enter"){
-        addBtn.click();
+function triggerClass(element, className, duration = 100) {
+    element.classList.add(className);
+    setTimeout(() => {
+        element.classList.remove(className);
+    }, duration);
+}
+
+function controlSystem(e){
+    let target;
+    let index;
+
+    if(e.path[1].className === 'controls'){
+        target = e.path[0];
+    } else if (e.path[2].className === 'controls'){
+        target = e.path[1];
+    } else {
+        return false;
+    }
+    
+    index = isOnIndex(target.parentElement.parentElement);
+
+    if(target.className === 'deleteBtn') {
+        deleteTask(index);
+    }
+
+    if(target.classList.contains('editBtn') || target.classList.contains('saveBtn')){
+        let label = todoList.children[index].children[1];
+        let formInput = todoList.children[index].children[2];
+
+        toggleClass([
+            target.parentElement.children[0],
+            target.parentElement.children[1],
+            label,
+            formInput,
+        ], 'hidden')
+
+        if(target.classList.contains('editBtn')) {
+            triggerEditTask(formInput);
+        }
+
+        if(target.classList.contains('saveBtn')) {
+            saveEditTask(index, label, formInput);
+        }
     }
 }
 
-
-function taskUpdate(index, innerData){
-	todoList.innerHTML +=
-	`<div class="listItem">
-		<div class="checkBoxDiv">
-			<input type="checkbox" class="checkbox" onchange="toggleDone()" id="item${index}" >
-		</div>
-		<label for="item${index}" class="label">${innerData}</label>			
-		<input type="text" class="labelInput hidden" onfocusout="unfocused(event)" value="${innerData}">
-		<div class="controls">
-			<button class="editBtn" onClick="editLabel(event)"><span class="fa fa-pen"></span></button>
-			<button class="saveBtn hidden" onClick="saveLabel(event)"><span class="fa fa-save"></span></button>
-			<button class="deleteBtn" onClick="removeListItem(event)"><span class="fa fa-trash"></span></button>
-		</div>
-	</div>`;
+function isOnIndex(element){
+    for(let i = 0; i < element.parentElement.childElementCount; i++){
+        if (element === element.parentElement.children[i]) {
+            return i;
+        }
+    }
 }
 
-
-function updateList(){
-	item = document.querySelectorAll(".listItem");
-	todoList = document.querySelector(".todoList");
+function updateAttributes(fromIndex){
+    for(let i = fromIndex; i < dataArray.length; i++){
+        renderToggleTask(i);
+        todoList.children[i].children[0].children[0].id = `item${i+1}`;
+        todoList.children[i].children[1].attributes.for.value =`item${i+1}`;
+    }
 }
 
-function toggleDone(){
-	for(var i = 0; i < todoList.childElementCount; i++){
-	    if(item[i].children[0].children[0].checked){
-			dataArray[i].isCompleted = true;
-	        item[i].children[1].classList.add("done");
-	    }
-	    else{
-			dataArray[i].isCompleted = false;
-	        item[i].children[1].classList.remove("done");
-	    }
-	}
-}
-
-function retick(){
-	for(var i = 0; i < item.length; i++){
-	    if(item[i].children[1].classList.contains("done") || dataArray[i].isCompleted){
-			item[i].children[0].children[0].checked = true;
-			item[i].children[1].classList.add("done");
-	    }
-	}
-}
-
-function updateItemIndex(){
-	for(var i = 0; i < item.length; i++){
-	    item[i].children[0].children[0].id = "item"+String(i+1);
-		item[i].children[1].setAttribute("for","item"+String(i+1)); 
-	}
-}
-
-
-function removeListItem(event){
-	if(!event.target.innerHTML){
-		dataArray.splice([event.path[3].children[0].children[0].id.slice(-1)-1], 1);
-		event.path[3].remove();
-	}
-	else{
-		dataArray.splice([event.path[2].children[0].children[0].id.slice(-1)-1,], 1);
-		event.path[2].remove();
-	}
-
-	updateList();
-	updateItemIndex();
-}
-
-
-function editLabel(event){
-	if(!event.target.innerHTML){
-	    capturedItem = event.path[3];
-	}
-	else{
-		capturedItem = event.path[2];
-	}
-
-	capturedItem.children[3].children[0].classList.add("hidden");
-	capturedItem.children[3].children[1].classList.remove("hidden");
-
-	capturedItem.children[2].value = capturedItem.children[1].innerText;
-
-	capturedItem.children[2].classList.remove("hidden");
-	capturedItem.children[1].classList.add("hidden");
-
-	capturedItem.children[2].focus();
-
-	capturedItem.children[2].selectionStart = capturedItem.children[1].innerText.length;
-
-}
-
-function saveLabel(event){
-	if(!event.target.innerHTML){
-	    capturedItem = event.path[3];
-	}
-	else{
-		capturedItem = event.path[2];
-	}
-
-	capturedItem.children[3].children[0].classList.remove("hidden");
-	capturedItem.children[3].children[1].classList.add("hidden");
-
-	capturedItem.children[2].classList.add("hidden");
-	capturedItem.children[1].classList.remove("hidden");
-
-	capturedItem.children[2].value = capturedItem.children[2].value.replace(/\s+/g, " ").replace(/^\s|\s$/g, "");
-
-	if( capturedItem.children[2].value && !(capturedItem.children[2].value === "  ") ){
-		capturedItem.children[1].innerText = capturedItem.children[2].value;
-		dataArray[capturedItem.children[0].children[0].id.slice(-1)-1].task = capturedItem.children[2].value;
-	}
-}
-
-function unfocused(event){
-	event.target.parentElement.children[3].children[1].click();
+function unfocused(e) {
+    triggerClass(e.target.nextElementSibling.children[1], 'active');
+    setTimeout(() => {
+        e.target.nextElementSibling.children[1].click();
+    }, 100)
 }
 
 document.documentElement.style.setProperty('--vh', `${visualViewport.height}px`);
