fbf91e834f3ae56e7447a20f69860886ce6efc35
SK <sksinghcodes@gmail.com>
2021-06-27 19:06:36 +0530

    update app

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-var dataArray = [];
-var addBtn = document.querySelector(".addBtn");
-var input = document.querySelector(".input");
-var todoList = document.querySelector(".todoList");
+const addBtn = document.querySelector(".addBtn");
+const input = document.querySelector(".input");
+const todoList = document.querySelector(".todoList");
+let dataArray = [];
+let taskBeingEditIndex;
+let saveBtnClicked = false;
 
-// check local storage and render data on the screen
 if(localStorage.length){
 	dataArray = JSON.parse(localStorage.getItem("dataString"));
     renderTasks(dataArray)
@@ -20,6 +21,10 @@ input.onkeypress = function(e){
 
 todoList.onclick = function(e){controlSystem(e);}
 
+function unfocused(){
+    saveTask();
+}
+
 function renderTasks(dataArray) {
     for(var i = 0; i < dataArray.length; i++ ){
 		renderSingleTask(dataArray[i].task, i + 1);
@@ -30,7 +35,7 @@ function renderSingleTask(task, index){
 	todoList.innerHTML +=
 	`<div class="listItem">
 		<div class="checkBoxDiv">
-			<input type="checkbox" class="checkbox" onchange="toggleTask(event)" id="item${index}" >
+			<input type="checkbox" class="checkbox" onchange="toggleTaskCompletion(event)" id="item${index}" >
 		</div>
 		<label for="item${index}" class="label">${task}</label>			
 		<input type="text" class="labelInput hidden" onfocusout="unfocused(event)" value="${task}">
@@ -42,23 +47,20 @@ function renderSingleTask(task, index){
 	</div>`;
 
     setTimeout(() => {
-        renderToggleTask(index - 1)
+        renderTaskCompletion(index - 1)
     }, 10);
 }
 
 function addNewTask(task){
-    // remove extra spaces from string
     var filteredTask = filterText(task);
 
     if(filteredTask){
-        // render task to the screen
-        renderSingleTask(filteredTask, dataArray.length);
-
-        // update the array
         dataArray.push({
             task: filteredTask,
             isCompleted: false,
         })
+
+        renderSingleTask(filteredTask, dataArray.length);
 
         saveChangesToLocal();
         input.value = "";
@@ -73,39 +75,55 @@ function deleteTask(index){
     updateAttributes(index);
 }
 
-function triggerEditTask(formInput){
-    formInput.focus();
-    formInput.selectionStart = formInput.value.length;
+function editTask(index){
+    let editField = todoList.children[index].children[2];
+    toggleClassHidden(index);
+    taskBeingEditIndex = index;
+    editField.focus();
+    editField.selectionStart = editField.value.length;
 }
 
-function saveEditTask(index, label, formInput){
-    let filteredTask = filterText(formInput.value);
-
-    if(filteredTask) {
-        label.innerText = filteredTask;
-        dataArray[index].task = filteredTask;
-        saveChangesToLocal();
-    }
+function saveTask(){
+    triggerClass(todoList.children[taskBeingEditIndex].children[3].children[1], 'active');
+    setTimeout(() => {
+        let editField = todoList.children[taskBeingEditIndex].children[2];
+        let filteredTask;
+        toggleClassHidden(taskBeingEditIndex);
+        filteredTask = filterText(editField.value);
+        if(filteredTask){
+            todoList.children[taskBeingEditIndex].children[1].innerText = filteredTask;
+            dataArray[taskBeingEditIndex].task = filteredTask;
+            saveChangesToLocal();
+        }
+        taskBeingEditIndex = null;
+    }, 100);
 }
 
-function toggleClass(elementArray, className){
+function toggleClassHidden(index){
+    let elementArray = [
+        todoList.children[index].children[1],
+        todoList.children[index].children[2],
+        todoList.children[index].children[3].children[0],
+        todoList.children[index].children[3].children[1]
+    ];
+
     for (let i = 0; i < elementArray.length; i++) {
-        if(elementArray[i].classList.contains(className)) {
-            elementArray[i].classList.remove(className);
+        if(elementArray[i].classList.contains('hidden')) {
+            elementArray[i].classList.remove('hidden');
         } else {
-            elementArray[i].classList.add(className);
+            elementArray[i].classList.add('hidden');
         }
     }
 }
 
-function toggleTask(e){
+function toggleTaskCompletion(e){
     let index = isOnIndex(e.path[2]);
     dataArray[index].isCompleted = !dataArray[index].isCompleted;
-    renderToggleTask(index);
+    renderTaskCompletion(index);
     saveChangesToLocal();
 }
 
-function renderToggleTask(index){
+function renderTaskCompletion(index){
     if(dataArray[index].isCompleted) {
         todoList.children[index].children[1].classList.add('done');
         todoList.children[index].children[0].children[0].checked = true;
@@ -150,26 +168,10 @@ function controlSystem(e){
 
     if(target.className === 'deleteBtn') {
         deleteTask(index);
-    }
-
-    if(target.classList.contains('editBtn') || target.classList.contains('saveBtn')){
-        let label = todoList.children[index].children[1];
-        let formInput = todoList.children[index].children[2];
-
-        toggleClass([
-            target.parentElement.children[0],
-            target.parentElement.children[1],
-            label,
-            formInput,
-        ], 'hidden')
-
-        if(target.classList.contains('editBtn')) {
-            triggerEditTask(formInput);
-        }
-
-        if(target.classList.contains('saveBtn')) {
-            saveEditTask(index, label, formInput);
-        }
+    } else if(target.className === 'editBtn') {
+        editTask(index);
+    } else if(target.className === 'saveBtn') {
+        saveTask(taskBeingEditIndex);
     }
 }
 
@@ -183,17 +185,10 @@ function isOnIndex(element){
 
 function updateAttributes(fromIndex){
     for(let i = fromIndex; i < dataArray.length; i++){
-        renderToggleTask(i);
+        renderTaskCompletion(i);
         todoList.children[i].children[0].children[0].id = `item${i+1}`;
         todoList.children[i].children[1].attributes.for.value =`item${i+1}`;
     }
-}
-
-function unfocused(e) {
-    triggerClass(e.target.nextElementSibling.children[1], 'active');
-    setTimeout(() => {
-        e.target.nextElementSibling.children[1].click();
-    }, 100)
 }
 
 document.documentElement.style.setProperty('--vh', `${visualViewport.height}px`);
